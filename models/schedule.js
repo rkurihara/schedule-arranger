294808f59ccddce661e47cac01b11fecd3ceeca6
Ryo Kurihara <lalalalyla.rk@gmail.com>
2018-07-01 01:23:42 +0900

    Scheduleのデータモデル定義

---
+++ models/schedule.js
@@ -0,0 +1,37 @@
+'use strict';
+const loader = require('./sequelize-loader');
+const Sequelize = loader.Sequelize;
+
+const Schedule = loader.database.define('schedules', {
+  scheudleId: {
+    type: Sequelize.UUID,
+    primaryKey: true,
+    allowNull: fasle
+  },
+  scheduleName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  memo: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  createdBy: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    allowNull: false
+  }
+}, {
+    freezeTableName: true,
+    timestamps: false,
+    indexes: [
+      {
+        fields: ['createdBy']
+      }
+    ]
+  });
+
+module.exports = Schedule;
