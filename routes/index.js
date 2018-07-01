d27c9acf827340a22a10dbb072b22a1364fe3863
rkurihara <lalalalyla.rk@gmail.com>
2018-07-01 06:08:09 +0000

    トップ画面に自分が作った予定一覧を作成

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const Schedule = require('../models/schedule');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express', user: req.user });
+  const title = "予定調整くん";
+  if (req.user) {
+    Schedule.findAll({
+      where: {
+        createdBy: req.user.id
+      },
+      order: '"updatedAt" DESC'
+    }).then((schedules) => {
+      res.render('index', {
+        title: title,
+        user: req.user,
+        schedules: schedules
+      });
+    });
+  } else {
+    res.render('index', { title: title, user: req.user });
+  }
 });
 
 module.exports = router;
