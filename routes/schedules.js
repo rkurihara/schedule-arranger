b3f1cb05307dc787117e539861ff4ce4b7f7d743
rkurihara <lalalalyla.rk@gmail.com>
2018-07-01 05:54:57 +0000

    予定作成フォームから送られた情報を保存

--- routes/schedules.js
+++ routes/schedules.js
@@ -2,14 +2,33 @@
 const expreess = require('express');
 const router = expreess.Router();
 const authenticationEnsurer = require('./authentication-ensurer');
+const uuid = require('node-uuid');
+const Schedule = require('../models/schedule');
+const Candidate = require('../models/candidate');
 
 router.get('/new', authenticationEnsurer, (req, res, next) => {
   res.render('new', { user: req.user });
 });
 
 router.post('/', authenticationEnsurer, (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
+  const scheduleId = uuid.v4();
+  const updatedAt = new Date();
+  Schedule.create({
+    scheduleId: scheduleId,
+    scheduleName: req.body.scheduleName.slice(0, 255),
+    memo: req.body.memo,
+    createdBy: req.user.id,
+    updatedAt: updatedAt
+  }).then((schedule) => {
+    const candidateNames = req.body.candidates.trim().split('\n').map((s) => s.trim());
+    const candidates = candidateNames.map((c) => { return {
+      candidateName: c,
+      scheduleId: schedule.scheduleId
+    };});
+    Candidate.bulkCreate(candidates).then(() => {
+      res.redirect('/schedules/' + schedule.scheduleId);
+    });
+  });
 });
 
 module.exports = router;
