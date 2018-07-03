312717c284ace556799e9155ea630b46c9a24245
rkurihara <lalalalyla.rk@gmail.com>
2018-07-03 15:23:32 +0000

    add test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,9 @@
 const request = require('supertest');
 const app = require('../app');
 const passportStub = require('passport-stub');
+let User = require('../models/user');
+let Schedule = require('../models/schedule');
+let Candidate = require('../models/candidate');
 
 describe('/login', () => {
   before(() => {
@@ -23,7 +26,7 @@ describe('/login', () => {
       .expect(200, done);
   });
 
-  it('ログインじはユーザ名が表示される', (done) => {
+  it('ログイン時はユーザ名が表示される', (done) => {
     request(app)
       .get('/login')
       .expect(/testuser/)
@@ -36,4 +39,50 @@ describe('/login', () => {
       .expect('Location', '/')
       .expect(302, done)
   });
-}); 
+});
+
+describe('/schedules', () => {
+  before(() => {
+    passportStub.install(app);
+    passportStub.login({ id: 0, username: 'testuser' });
+  });
+
+  after(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  it('予定が作成でき、表示される', (done) => {
+    User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+      request(app)
+        .post('/schedules')
+        .send({ scheduleName: 'テスト予定1', memo: 'テストメモ1\r\nテストメモ2', candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3' })
+        .expect('Location', /schedules/)
+        .expect(302)
+        .end((err, res) => {
+          let createdSchedulePath = res.headers.location;
+          request(app)
+            .get(createdSchedulePath)
+            .expect(/テスト予定1/)
+            .expect(/テストメモ1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト候補1/)
+            .expect(/テスト候補2/)
+            .expect(/テスト候補3/)
+            .expect(200)
+            .end((err, res) => {
+              // テストで作成したデータを削除
+              let scheduleId = createdSchedulePath.split('/schedules/')[1];
+              Candidate.findAll({
+                where: { scheduleId: scheduleId }
+              }).then((candidates) => {
+                candidates.forEach((c) => { c.destroy(); });
+                Schedule.findById(scheduleId).then((s) => { s.destroy(); });
+              });
+              if (err) return done(err);
+              done();
+            });
+        });
+    });
+  });
+});
