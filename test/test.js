6b6248ce2f1eb464b258bda018d7bb33064d66a9
rkurihara <lalalalyla.rk@gmail.com>
2018-07-08 15:17:35 +0000

    refactor test

--- test/test.js
+++ test/test.js
@@ -162,25 +162,27 @@ describe('/schedules/:scheduleId/users/:userId/comments', () => {
 });
 
 function deleteScheduleAggregate(scheduleId, done, err) {
-  const processCommentDestory = Comment.findAll({
+  const promiseCommentDestroy = Comment.findAll({
     where: { scheduleId: scheduleId }
-  }).then((comments) => { comments.map((c) => { return c.destroy(); }); });
+  }).then((comments) => { 
+    return Promise.all(comments.map((c) => { return c.destroy(); })); 
+  });
 
   Availability.findAll({
     where: { scheduleId: scheduleId }
   }).then((availabilities) => {
     const promises = availabilities.map((a) => { return a.destroy(); });
-    Promise.all(promises).then(() => {
-      Candidate.findAll({
-        where: { scheduleId: scheduleId }
-      }).then((candidates) => {
-        const promises = candidates.map((c) => { return c.destroy(); });
-        Promise.all(promises).then(() => {
-          Schedule.findById(scheduleId).then((s) => { s.destroy(); });
-          if (err) return done(err);
-          done();
-        });
-      });
+    return Promise.all(promises);
+  }).then(() => {
+    return Candidate.findAll({
+      where: { scheduleId: scheduleId }
     });
+  }).then((candidates) => {
+    const promises = candidates.map((c) => { return c.destroy(); });
+    promises.push(promiseCommentDestroy);
+  }).then(() => {
+    Schedule.findById(scheduleId).then((s) => { s.destroy(); });
+    if (err) return done(err);
+    done();
   });
 }
