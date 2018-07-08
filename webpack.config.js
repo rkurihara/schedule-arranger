c5ad643fed7fa382c912c517dc0380f5ee8ff435
rkurihara <lalalalyla.rk@gmail.com>
2018-07-08 11:26:16 +0000

    AJAX による出欠の更新

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  context: __dirname + '/app',
+  entry: './entry',
+  output: {
+    path: __dirname + '/public/javascripts',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    }]
+  }
+};
