d8e1b1aadebca3dd8600acf294b1634142f83150
rkurihara <lalalalyla.rk@gmail.com>
2018-07-14 06:12:27 +0000

    予定に関連する全ての情報が削除できるテスト追加

--- test/test.js
+++ test/test.js
@@ -203,3 +203,92 @@ describe('/schedules/:scheduleId?edit=1', () => {
     });
   });
 });
+
+describe('/schedules/:scheduleId?delete=1', () => {
+  before(() => {
+    passportStub.install(app);
+    passportStub.login({ id: 0, username: 'testuser' });
+  });
+
+  after(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  it('予定に関連する全ての情報が削除できる', (done) => {
+    User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+      request(app)
+        .post('/schedules')
+        .send({ scheduleName: 'テスト更新予定1', memo: 'テスト更新メモ1', candidates: 'テスト更新候補1' })
+        .end((err, res) => {
+          const createdSchedulePath = res.headers.location;
+          const scheduleId = createdSchedulePath.split('/schedules/')[1];
+
+          // 出欠作成
+          const promiseAvailability = Candidate.findOne({
+            where: { scheduleId: scheduleId }
+          }).then((candidate) => {
+            return new Promise((resolve) => {
+              request(app)
+                .post(`/schedules/${scheduleId}/users/${0}/candidates/${candidate.candidateId}`)
+                .send({ availability: 2 }) // 出席に更新
+                .end((err, res) => {
+                  if (err) done(err);
+                  resolve();
+                });
+            });
+          });
+
+          // コメント作成
+          const promiseComment = new Promise((resolve) => {
+            request(app)
+              .post(`/schedules/${scheduleId}/users/${0}/comments`)
+              .send({ comment: 'testcomment' })
+              .expect('{"status":"OK","comment":"testcomment"}')
+              .end((err, res) => {
+                if (err) done(err);
+                resolve();
+              });
+          });
+
+          // 削除
+          const promiseDeleted = Promise.all([promiseAvailability, promiseComment]).then(() => {
+            return new Promise((resolve) => {
+              request(app)
+                .post(`/schedules/${scheduleId}?delete=1`)
+                .end((err, res) => {
+                  if (err) done(err);
+                  resolve();
+                });
+            });
+          });
+
+          // テスト
+          promiseDeleted.then(() => {
+            const p1 = Comment.findAll({
+              where: { scheduleId: scheduleId }
+            }).then((comments) => {
+              assert.equal(comments.length, 0);
+            });
+            const p2 = Availability.findAll({
+              where: { scheduleId: scheduleId }
+            }).then((availabilities) => {
+              assert.equal(availabilities.length, 0);
+            });
+            const p3 = Candidate.findAll({
+              where: { scheduleId: scheduleId }
+            }).then((candidates) => {
+              assert.equal(candidates.length, 0);
+            });
+            const p4 = Schedule.findById(scheduleId).then((schedule) => {
+              assert.equal(!schedule, true);
+            });
+            Promise.all([p1, p2, p3, p4]).then(() => {
+              if (err) return done(err);
+              done();
+            });
+          });
+        });
+    });
+  });
+});
