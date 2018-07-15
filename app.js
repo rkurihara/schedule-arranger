bed24c379fdefc2f0c56574841627247f867fd42
rkurihara <lalalalyla.rk@gmail.com>
2018-07-15 01:40:13 +0000

    リダイレクト機能追加

--- app.js
+++ app.js
@@ -96,8 +96,18 @@ app.get('/auth/github',
 app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
   function (req, res) {
-    res.redirect('/');
-  });
+    var loginFrom = req.cookies.loginFrom;
+    // オープンリダイレクタ脆弱性対策
+    if (loginFrom &&
+      loginFrom.indexOf('http://') < 0 &&
+      loginFrom.indexOf('https://') < 0) {
+      res.clearCookie('loginForm');
+      res.redirect(loginFrom);
+    } else {
+      res.redirect('/');
+    }
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/authentication-ensurer.js
+++ routes/authentication-ensurer.js
@@ -2,7 +2,7 @@
 
 function ensure(req, res, next) {
   if (req.isAuthenticated()) { return next() }
-  res.redirect('/login');
+  res.redirect('/login?from=' + req.originalUrl);
 };
 
 module.exports = ensure;
